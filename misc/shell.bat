7d04cfe91f4859c0f04c75e3680fd6f3b1f46ad5
Ian Lanning <ianclanning@gmail.com>
2017-09-30 22:02:05 -0700

    Initial commit, starting from the Handmade Hero day 25 source code

---
+++ misc/shell.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM  To run this at startup, use this as your shortcut target:
+REM  %windir%\system32\cmd.exe /k w:\handmade\misc\shell.bat
+
+call "F:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+set path=%~dp0;%path%
+set exe_path=%~dp0
+set exe_path=%exe_path:~0,-5%build\win32_handmade.exe
+call devenv "%exe_path%"
